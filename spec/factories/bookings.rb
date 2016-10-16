d42bdde771f8937a8d878ffaf2326263497da655
andela-amagana <alex.magana@andela.com>
2016-10-16 16:23:58 +0300

    Remove 'user_id' to allow dynamic assignment

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :booking do
     booking_code Faker::Code.asin
     flight_id Faker::Number.number(2)
-    user_id 1
     email "precious.thiel@murazikbalistreri.name"
     flight
   end
