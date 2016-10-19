a4964a4c6470a6c59b38ea35db47fc3383c9ff30
andela-amagana <alex.magana@andela.com>
2016-10-19 12:31:23 +0300

    Include user_id in factory

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     booking_code Faker::Code.asin
     flight_id Faker::Number.number(2)
     email "precious.thiel@murazikbalistreri.name"
+    user_id Faker::Number.number(2)
     flight
   end
 end
