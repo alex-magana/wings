438fa47af33cde19a9312c3caddfae339367d2e5
andela-amagana <alex.magana@andela.com>
2016-10-19 12:39:15 +0300

    Add search_flight and add_booking_details in module Helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,31 +1,33 @@
 module Helpers
-  def login_as(user)
-    authenticate(user.email, user.password)
-    request.session[:user_id] = user.id
-  end
-
-  def authenticate(email, password)
-    @user = User.find_by_email(email)
-    @user.authenticate(password)
-  end
-
-  def sign_in_with(email, password)
-    visit login_path
-    fill_in 'email', with: email
-    fill_in 'password', with: password
-    click_button 'Log In'
-  end
-
-  def sign_up_with(name, email, password, password_confirmation)
+  def search_flights
     visit root_path
-    click_link 'Signup'
-    fill_in 'user_name', with: name
-    fill_in 'user_email', with: email
-    fill_in 'user_password', with: password
-    fill_in 'user_password_confirmation', with: password_confirmation
-    click_button 'Sign up'
+    find_all("div.select-wrapper input").first.click
+    sleep(0.3)
+    find("div.select-wrapper li", text: 'Goroka').click
+    find_all("div.select-wrapper input")[1].click
+    sleep(0.3)
+    find("div.select-wrapper li", text: 'Madang').click
+    find_all("div.select-wrapper input")[2].click
+    sleep(0.3)
+    find("div.select-wrapper li", text: '3').click
+    page.execute_script("$('.datepicker')\
+                        .pickadate('picker').set('select', '22 October, 2016')")
+    click_button('search')
   end
-
+  def add_booking_details
+    sleep(0.3)
+    selected_flight = find_all("table tbody tr td input").first
+    sleep(0.3)
+    selected_flight.click
+    click_button('select-flight')
+    fill_in("booking_email", with: "gatobumalex@gmail.com")
+    find_all('div.links a').first.click
+    find_all("div.nested-fields div.field input").first.set("ANDREW WHITE")
+    find_all("div.nested-fields div.field input")[1].set("WQ23DFG324D")
+    sleep(0.3)
+    find_all("div.actions input").first.click
+    sleep(0.3)
+  end
   def stub_current_user(user)
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(user)
