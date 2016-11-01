0ea3cee1d98b67fbf7c63403dcb5631eacb3fc02
andela-amagana <alex.magana@andela.com>
2016-11-01 20:15:51 +0300

    Use lambda for multiline scopes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :flight, inverse_of: :bookings
   has_many :passengers, inverse_of: :booking
-  accepts_nested_attributes_for :passengers, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :passengers,
+                                reject_if: :all_blank, allow_destroy: true
 
   VALID_REGEX_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
@@ -18,21 +19,21 @@ class Booking < ApplicationRecord
             presence: true,
             format: { with: VALID_REGEX_EMAIL }
 
-  scope :past_bookings, -> (params) do
-    joins("INNER JOIN flights ON flights.id = bookings.flight_id")
-    .where("bookings.user_id = ?", params.id)
-    .select("bookings.*, flights.flight_code, flights.airline_code,\
+  scope :past_bookings, (lambda do |params|
+    joins("INNER JOIN flights ON flights.id = bookings.flight_id").
+    where("bookings.user_id = ?", params.id).
+    select("bookings.*, flights.flight_code, flights.airline_code,\
      flights.departure_date, flights.arrival_date")
-  end
+  end)
 
-  scope :search_booking_write, -> (params) do
+  scope :search_booking_write, (lambda do |params|
     where("bookings.booking_code = ?", params[:booking_code])
-  end
+  end)
 
-  scope :search_booking_read, -> (params) do
-    joins("INNER JOIN flights ON flights.id = bookings.flight_id")
-    .where("bookings.booking_code = ?", params[:booking_code])
-    .select("bookings.*, flights.flight_code, flights.airline_code,\
+  scope :search_booking_read, (lambda do |params|
+    joins("INNER JOIN flights ON flights.id = bookings.flight_id").
+    where("bookings.booking_code = ?", params[:booking_code]).
+    select("bookings.*, flights.flight_code, flights.airline_code,\
      flights.departure_date, flights.arrival_date")
-  end
+  end)
 end
