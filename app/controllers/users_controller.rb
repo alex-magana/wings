422f925e39a8a3b15903c64658cdb9b62e2778b8
andela-amagana <alex.magana@andela.com>
2016-11-04 10:43:22 +0300

    Refactor users_controller
    Adhere to the style guide

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,71 +3,96 @@ class UsersController < ApplicationController
 
   def show
   end
+
   def new
     @user = User.new
   end
+
   def create
-    @user_with_email = User.check_email(user_params).first
+    @user_with_email = User.check_email(to_uppercase(user_params)).first
     if @user_with_email.nil? == false
-      flash[:notice] = 'The email provided is already in use.'
+      flash[:notice] = "The email provided is already in use."
       redirect_to new_user_path
     else
-      @user = User.new(user_params)
+      @user = User.new(to_uppercase(user_params))
       @user.role_id = 2
-      respond_to do |format|
-        if @user.save
-          format.html { redirect_to login_path, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @user }
-        else
-          format.html { redirect_to new_user_path }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+      if @user.save
+        set_session_user(@user)
+        redirect_to flights_path, notice: "User was successfully created."
+      else
+        redirect_to new_user_path
       end
     end
   end
+
   def login
   end
+
   def user_authenticate
-    @user = User.user_authenticate(login_params).first
+    @user = User.user_authenticate(to_uppercase(login_params)).first
     if @user.nil? == false
-      session[:user_id] = @user.id
+      set_session_user(@user)
       redirect_to flights_path
     else
-      flash[:notice] = 'Invalid username or password.'
+      flash[:notice] = "Invalid username or password."
       redirect_to login_path
     end
   end
+
   def logout
-    session[:user_id] = nil
+    terminate_session
     redirect_to root_path
   end
+
   def reset_password
   end
+
   def send_reset_email
-    @user = User.check_email(user_params).first
+    @user = User.check_email(to_uppercase(reset_params)).first
     if @user.nil? == false
       @user.password = SecureRandom.hex(7).upcase
       if @user.save
         AppMailer.password_send(@user).deliver_now
-        flash[:notice] = 'Password reset successfully. Please check your email.'
+        flash[:notice] = "Password reset successfully. Please check your email."
         redirect_to login_path
       else
-        flash[:notice] = 'Unable to reset password. Try again later.'
+        flash[:notice] = "Unable to reset password. Try again later."
         redirect_to reset_password_path
       end
     else
-      flash[:notice] = 'Email does not exist.'
+      flash[:notice] = "Email does not exist."
       redirect_to reset_password_path
     end
   end
+
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
-    def user_params
-      params.require(:user).permit(:first_name, :middle_name, :last_name, :email, :password)
-    end
-    def login_params
-      params.permit(:email, :password)
-    end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_session_user(user)
+    session[:user_id] = user.id
+  end
+
+  def terminate_session
+    session[:user_id] = nil
+  end
+
+  def to_uppercase(params)
+    params.each_pair { |_key, value| value.upcase! }
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :middle_name, :last_name,
+                                 :email, :password)
+  end
+
+  def login_params
+    params.permit(:email, :password)
+  end
+
+  def reset_params
+    params.permit(:email)
+  end
 end
