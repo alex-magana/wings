f77921322606651ef5b0ebae552ed025dde372da
andela-amagana <alex.magana@andela.com>
2016-11-04 12:08:12 +0300

    Refactor feature specs

--- spec/features/anonymous_user_spec.rb
+++ spec/features/anonymous_user_spec.rb
@@ -1,34 +1,36 @@
-require 'rails_helper'
+require "rails_helper"
 
-Capybara.default_wait_time = 15
-
-RSpec.feature 'AnonymousUserUsesTheApp', js: true do
-  scenario 'User visits the website' do
+RSpec.feature "AnonymousUserUsesTheApp", js: true do
+  scenario "User visits the website" do
     visit root_path
     expect(find_all("ul.main-nav li.top-right a").first).to have_content("Sign In")
     expect(find_all("ul.main-nav li.top-right a")[1]).to have_content("Sign Up")
     expect(page).to_not have_content("Past bookings")
   end
-  scenario 'User searches for a flight' do
+
+  scenario "User searches for a flight" do
     search_flights
     sleep(0.3)
-    expect(page).to have_content('AIRLINE')
+    expect(page).to have_content("AIRLINE")
   end
-  scenario 'User selects a flight' do
+
+  scenario "User selects a flight" do
     search_flights
     sleep(0.3)
     selected_flight = find_all("table tbody tr td input").first
     sleep(0.3)
     selected_flight.click
-    click_button('select-flight')
+    click_button("pickoption")
     expect(page).to have_content("ADD PASSENGER")
   end
-  scenario 'User adds booking details' do
+
+  scenario "User adds booking details" do
     search_flights
     add_booking_details
     expect(find_all("div.actions input").first.value).to eq "CONFIRM"
   end
-  scenario 'User confrims booking' do
+
+  scenario "User confirms booking" do
     search_flights
     add_booking_details
     find_all("div.actions input").first.click
--- spec/features/registered_user_spec.rb
+++ spec/features/registered_user_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'RegisteredUserUsesTheApp', js: true do
+RSpec.feature "RegisteredUserUsesTheApp", js: true do
+  let(:user_valid) { User.find(3) }
 
+  let(:user_invalid) do
+    create :user, email: "precious.thiel.mann@murazik.com",
+                  password: "1234567890"
+  end
+
+  context "User logs in" do
+    scenario "with valid credentials" do
+      visit login_path
+      fill_in "email", with: user_valid.email
+      fill_in "password", with: user_valid.password
+      find("div.input-field #submit").click
+      expect(page).to have_content("Log Out")
+    end
+
+    scenario "with invalid credentials" do
+      visit login_path
+      fill_in "email", with: user_invalid.email
+      fill_in "password", with: user_invalid.password
+      find("div.input-field #submit").click
+      expect(page).to have_content("Invalid username or password.")
+    end
+  end
 end
