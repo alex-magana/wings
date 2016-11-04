1f334b9e071466addbce38076d90d4e79170c079
andela-amagana <alex.magana@andela.com>
2016-11-04 10:34:51 +0300

    Add method to provide current user's id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_user_id
+    current_user.id if current_user
+  end
+
   def require_login
     unless current_user
       flash[:error] = "LOG IN REQUIRED"
