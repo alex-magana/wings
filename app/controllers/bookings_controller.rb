c48510e32dae4028ca67c442867ef222d98fb64d
andela-amagana <alex.magana@andela.com>
2016-11-04 10:35:59 +0300

    Refactor bookings_controller
    Eliminate repetition and adhere to the style guide

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,111 +1,95 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:create]
 
   def index
-    @bookings = Booking.all
-  end
-  def show
-  end
-  def new
-    @flight = Flight.find(new_booking_params[:flight_group].to_i)
-    @booking = Booking.new
-    @booking.flight = @flight
-    @booking.booking_code = SecureRandom.hex(4).upcase
-    if current_user
-      @booking.email = current_user.email
-      @booking.user_id = current_user.id
-    else
-      @booking.user_id = 2
+    @bookings = Booking.find_by_user(current_user)
+    if @bookings.blank? || @bookings.nil?
+      flash[:notice] = "There are no past bookings."
     end
   end
+
+  def show
+  end
+
+  def new
+    flight = Flight.find(new_booking_params[:flight_group].to_i)
+    @booking = flight.bookings.new
+    new_booking_params[:passengers_qty].to_i.times { @booking.passengers.build }
+    @booking.email = current_user.email if current_user
+  end
+
   def edit
   end
+
   def create
     @booking = Booking.new(booking_params)
     @flight = Flight.find(booking_params[:flight_id])
     @flight.available_seats = @flight.available_seats - @booking.passengers.size
-    respond_to do |format|
-      if @booking.save
-        if @flight.save
-          # Tell the AppMailer to send an email after save
-          AppMailer.message_send(@booking).deliver_now
-          format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
-          format.json { render :show, status: :created, location: @booking }
-        else
-          format.html { render :new }
-          format.json { render json: @booking.errors, status: :unprocessable_entity }
-        end
-      else
-        format.html { render :new }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
+    if @booking.save && @flight.save
+      AppMailer.message_send(@booking).deliver_now
+      redirect_to @booking, notice: "Booking was successfully created."
+    else
+      render :new
     end
   end
+
   def update
-    respond_to do |format|
-      if @booking.update(booking_params)
-        # Tell the AppMailer to send an email after update
-        AppMailer.message_send(@booking).deliver_now
-        format.html { redirect_to @booking, notice: 'Booking was successfully updated.' }
-        format.json { render :show, status: :ok, location: @booking }
-      else
-        format.html { render :edit }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
+    if @booking.update(booking_params)
+      AppMailer.message_send(@booking).deliver_now
+      redirect_to booking_path(@booking), notice:
+                                          "Booking was successfully updated."
+    else
+      render :edit
     end
   end
+
   def destroy
     @booking.destroy
-    respond_to do |format|
-      format.html { redirect_to bookings_url, notice: 'Booking was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to bookings_path, notice:
+                                    "Booking was successfully destroyed."
   end
+
   def confirm_booking
     @booking = Booking.new(booking_params)
   end
-  def past_bookings
-    @bookings = Booking.past_bookings(current_user)
-    if @booking.nil? == false
-      render '_past_bookings'
-    else
-      flash[:notice] = "There are no past bookings."
-      render '_past_bookings'
-    end
-  end
+
   def manage_bookings
-    render '_search_booking'
+    render "_search_booking"
   end
+
   def search_booking
-    if current_user
-      @booking = Booking.search_booking_write(search_booking_params).first
-      if @booking.nil? == false
-        render 'edit'
-      else
-        flash[:notice] = "No reservations available."
-        redirect_to manage_bookings_path
-      end
+    @booking = Booking.find_by_booking_code(search_params[:booking_code])
+    if @booking.blank? == false && @booking.nil? == false
+      render "_search_results"
     else
-      @booking = Booking.search_booking_read(search_booking_params).first
-      if @booking.nil? == false
-        render '_search_results'
-      else
-        flash[:notice] = "No reservations available."
-        redirect_to manage_bookings_path
-      end
+      flash[:notice] = "No reservations available."
+      redirect_to manage_bookings_path
     end
   end
+
   private
-    def new_booking_params
-      params.permit(:flight_group)
-    end
-    def booking_params
-      params.require(:booking).permit(:user_id, :flight_id, :booking_code, :email, passengers_attributes: [:id, :passenger_name, :passport_number, :_destroy])
-    end
-    def search_booking_params
-      params.permit(:booking_code)
-    end
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
+
+  def new_booking_params
+    params.permit(:passengers_qty, :flight_group)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :flight_id, :email,
+                                    passengers_attributes:
+                                    [:id, :passenger_name,
+                                     :passport_number, :_destroy])
+  end
+
+  def search_params
+    params.permit(:booking_code)
+  end
+
+  def set_user
+    params[:booking][:user_id] = current_user_id || 2
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
