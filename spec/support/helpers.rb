42cba6f7ae88978a5ae4292f5c4af1c373e2b035
andela-amagana <alex.magana@andela.com>
2016-11-04 12:12:04 +0300

    Refactor helper methods

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,33 +3,34 @@ module Helpers
     visit root_path
     find_all("div.select-wrapper input").first.click
     sleep(0.3)
-    find("div.select-wrapper li", text: 'Goroka').click
+    find("div.select-wrapper li", text: "Goroka").click
     find_all("div.select-wrapper input")[1].click
     sleep(0.3)
-    find("div.select-wrapper li", text: 'Madang').click
+    find("div.select-wrapper li", text: "Madang").click
     find_all("div.select-wrapper input")[2].click
     sleep(0.3)
-    find("div.select-wrapper li", text: '3').click
-    page.execute_script("$('.datepicker')\
-                        .pickadate('picker').set('select', '22 October, 2016')")
-    click_button('search')
+    find("div.select-wrapper li", text: "1").click
+    page.execute_script("$('.datepicker').pickadate('picker')."\
+                        "set('select', '22 October, 2016')")
+    click_button("submit")
   end
+
   def add_booking_details
     sleep(0.3)
-    selected_flight = find_all("table tbody tr td input").first
+    selected_flight = find_all("input.flight_option").first
     sleep(0.3)
     selected_flight.click
-    click_button('select-flight')
+    find("div.col #submit_option").click
     fill_in("booking_email", with: "gatobumalex@gmail.com")
-    find_all('div.links a').first.click
     find_all("div.nested-fields div.field input").first.set("ANDREW WHITE")
     find_all("div.nested-fields div.field input")[1].set("WQ23DFG324D")
     sleep(0.3)
     find_all("div.actions input").first.click
     sleep(0.3)
   end
+
   def stub_current_user(user)
-    allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).and_return(user)
   end
 end
